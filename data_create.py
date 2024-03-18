e0836630234aa2ec6f984e09335ee41d918c5ee4
HardikArora17 <54908862+HardikArora17@users.noreply.github.com>
2024-03-18 21:04:02 +0530

    Update data_create.py

--- data_create.py
+++ data_create.py
@@ -2,21 +2,13 @@ from tqdm.autonotebook import tqdm
 import pandas as pd
 
 
-def create_data(file_name):
-    qna_data = pd.read_csv(file_name)
-    abstracts = qna_data['abstract']
-    questions = qna_data['question']
-    answers = qna_data['answer']
-
+def create_data(dataset):
     instruct_data = []
 
-    for abstract, ques, answer in tqdm(zip(abstracts, questions, answers), desc="READING DATA..."):
-        try:
-            instruction = f"Study the whole scientific abstract given below and try to answer the given question based on abstract.\n[ABSTRACT]: {abstract}\n [QUESTION]: {ques}"
-            format_llm = f"<s> [INST] {instruction} [/INST] {answer} </s>"
-            instruct_data.append(format_llm)
-        except:
-            continue
+    # for type_of_split in dataset:
+    #   print(type_of_split)
+    for row in tqdm(dataset['train']):
+      instruct_data.append(row['text'])
 
     print("TOTAL TRAINING SAMPLES GATHERED: ", len(instruct_data))
 
