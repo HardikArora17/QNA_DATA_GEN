0dd93a29daa88cee28aa05a7a84a669a7c9c6ff7
HardikArora17 <54908862+HardikArora17@users.noreply.github.com>
2024-04-08 22:43:35 +0530

    Update finetuning_main_qwen.py

--- finetuning_main_qwen.py
+++ finetuning_main_qwen.py
@@ -4,13 +4,12 @@ from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training, get_pef
 from trl import SFTTrainer
 from datasets import Dataset
 import pandas as pd
-from data_create import create_data
 from global_variables_qwen import *
 import os
 from datasets import load_dataset
 
 
-def run_finetuning(data_filename, model_name, new_model_name, output_path):
+def run_finetuning(dataset, model_name, new_model_name, output_path):
 
     # Load tokenizer and model with QLoRA configuration
     compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
@@ -22,33 +21,31 @@ def run_finetuning(data_filename, model_name, new_model_name, output_path):
         bnb_4bit_use_double_quant=use_nested_quant,
     )
 
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        quantization_config=bnb_config)
+    tokenizer_path = model_name
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
+    tokenizer.add_eos_token = True
 
-    # model = model.to('cuda:1')
+    print("tokenizer done")
+    
+    def tokenize_function(examples):
+      return tokenizer(examples["text"], truncation=True)
+
+    train_dataset = dataset.shuffle().map(tokenize_function, batched=True)
+    print("DATA_LOADED")
+
+    model_path = model_name
+    model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=bnb_config)
+    model = model.to('cuda')
     print("MODEL INITIALIZED")
     print("Model's parameters device:", next(model.parameters()).device)
 
     # Alternatively, you can print the device of the model itself
     print("Model's device:", next(model.parameters()).device)
 
-    instruct_data = create_data(data_filename)
-
-    data = pd.DataFrame({'text': instruct_data})
-    print("DATA_LOADED")
-
-    # Convert the DataFrame to a Hugging Face Dataset
-    dataset = Dataset.from_pandas(data)
-
     model.config.use_cache = False
     model.config.pretraining_tp = 1
-
-    # Load LLaMA tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
-    tokenizer.add_eos_token = True
 
     # Load LoRA configuration
     peft_config = LoraConfig(
@@ -70,15 +67,16 @@ def run_finetuning(data_filename, model_name, new_model_name, output_path):
         logging_steps=logging_steps,
         learning_rate=learning_rate,
         weight_decay=weight_decay,
-        fp16=fp16,
-        optim=optim,
+        optim='adamw_hf',
         max_grad_norm=max_grad_norm,
         max_steps=max_steps,
         warmup_ratio=warmup_ratio,
         group_by_length=group_by_length,
-        lr_scheduler_type=lr_scheduler_type)
+        lr_scheduler_type=lr_scheduler_type,
+        report_to="tensorboard")
 
     print("LOADED TRAINING ARGUMENTS")
+    
     # Set supervised fine-tuning parameters
     trainer = SFTTrainer(
         model=model,
@@ -96,7 +94,7 @@ def run_finetuning(data_filename, model_name, new_model_name, output_path):
 
     trainer.train()
     # Save trained model
-    trainer.model.save_pretrained(f'{output_path}/{new_model_name}')
+    trainer.model.save_pretrained( f'{output_path}/{new_model_name}')
 
     model_state_dict = model.state_dict()
     torch.save(model_state_dict, f'{output_path}/{new_model_name}_state.pt')
@@ -104,15 +102,17 @@ def run_finetuning(data_filename, model_name, new_model_name, output_path):
     print("Model state dictionary saved.")
 
 
-
 if __name__ == '__main__':
-    model_name = 'Qwen/Qwen1.5-1.8B'
-    dataset = load_dataset("universeTBD/arxiv-astro-abstracts-all")
-    new_model_name = 'finetuned_qwen_astro_data'
-    output_file_path = 'output_finetuned_model'
+    model_name = 'microsoft/phi-2'
+    dataset_name = 'universeTBD/arxiv-astro-abstracts-all'
+    dataset_path = os.path.join(dataset_name)
+    dataset = load_dataset(dataset_path)['train'].select(range(100))
+    
+    new_model_name = 'finetuned_astr_phi2'
+    output_file_path = 'stored_output_model'
     
     if not os.path.exists(output_file_path):
-        os.makedirs(output_file_path)
+        os.makedirs(output_file_path, exist_ok = True)
     
     print("directories_made")
     run_finetuning(dataset, model_name, new_model_name, output_file_path)
