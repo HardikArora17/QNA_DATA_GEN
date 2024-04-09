0ae78f276bd9bd0bbd5a019735e842196104dbaa
HardikArora17 <54908862+HardikArora17@users.noreply.github.com>
2024-04-09 19:57:47 +0530

    Update data_create.py

--- data_create.py
+++ data_create.py
@@ -1,15 +1,24 @@
-from tqdm.autonotebook import tqdm
+import os
+from tqdm import tqdm
 import pandas as pd
+from datasets import Dataset
+
+def create_data(input_data_path, data_output_path):
+  data_folder  = input_data_path
+  list_of_files = os.listdir(data_folder)
+  example = []
+
+  for file_name in tqdm(list_of_files):
+    with open (os.path.join(data_folder, file_name), 'r') as file:
+      lines = file.readlines()
+      lines = " ".join(lines)
+      example.append({'text': lines})
+
+  df_data = pd.DataFrame(example)
+  dataset = Dataset.from_pandas(df_data)
+  dataset.save_to_disk(data_output_path)
 
 
-def create_data(dataset):
-    instruct_data = []
-
-    # for type_of_split in dataset:
-    #   print(type_of_split)
-    for row in tqdm(dataset['train']):
-      instruct_data.append(row['text'])
-
-    print("TOTAL TRAINING SAMPLES GATHERED: ", len(instruct_data))
-
-    return instruct_data
+input_data_path = '/content/train_data'
+output_data_path = 'astro_full_text_data'
+create_data(input_data_path, output_data_path)
