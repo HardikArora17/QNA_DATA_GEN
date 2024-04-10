cd224ec4008b64f09e52181ec5b937bac80f62c6
HardikArora17 <54908862+HardikArora17@users.noreply.github.com>
2024-04-10 19:13:57 +0530

    Create finetuning_sft_phi2_peft.py

---
+++ finetuning_sft_phi2_peft.py
@@ -0,0 +1,143 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments
+from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training, get_peft_model
+from trl import SFTTrainer
+from datasets import Dataset
+import pandas as pd
+from global_variables_phi2 import *
+import os
+from datasets import load_dataset, load_from_disk
+
+
+def run_finetuning(dataset, model_name, new_model_name, output_path):
+
+    # Load tokenizer and model with QLoRA configuration
+    compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=use_4bit,
+        bnb_4bit_quant_type=bnb_4bit_quant_type,
+        bnb_4bit_compute_dtype=compute_dtype,
+        bnb_4bit_use_double_quant=use_nested_quant,
+    )
+
+    tokenizer_path = model_name
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
+    tokenizer.add_eos_token = True
+
+    print("tokenizer done")
+    
+    def tokenize_function(examples):
+      return tokenizer(examples["text"], truncation=True)
+
+    train_dataset = dataset #.shuffle().map(tokenize_function, batched=True)
+    print("DATA_LOADED")
+
+    model_path = model_name
+    model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=bnb_config, device_map="auto")
+    # model = model.to('cuda')
+    print("MODEL INITIALIZED")
+    print("Model's parameters device:", next(model.parameters()).device)
+
+    # Alternatively, you can print the device of the model itself
+    print("Model's device:", next(model.parameters()).device)
+
+    model.config.use_cache = False
+    model.config.pretraining_tp = 1
+
+    # Load LoRA configuration
+    peft_config = LoraConfig(
+        lora_alpha=lora_alpha,
+        lora_dropout=lora_dropout,
+        r=lora_r,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules = [
+        "Wqkv",
+        "fc1",     # Targeting the first fully connected layer in PhiMLP
+        "fc2"     # Targeting the second fully connected layer in PhiMLP]
+        ])
+
+    # Set training parameters
+    training_arguments = TrainingArguments(
+        output_dir=output_path,
+        num_train_epochs=num_train_epochs,
+        per_device_train_batch_size=per_device_train_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        save_steps=save_steps,
+        logging_steps=logging_steps,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        optim=optim,
+        max_grad_norm=max_grad_norm,
+        max_steps=max_steps,
+        warmup_ratio=warmup_ratio,
+        group_by_length=group_by_length,
+        lr_scheduler_type=lr_scheduler_type,
+        report_to="tensorboard")
+
+    print("LOADED TRAINING ARGUMENTS")
+    
+    # Set supervised fine-tuning parameters
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=dataset,
+        peft_config=peft_config,
+        dataset_text_field="text",
+        max_seq_length= tokenizer.model_max_length,
+        tokenizer=tokenizer,
+        args=training_arguments,
+        packing=packing
+    )
+
+    # Train model
+    print("training_started")
+
+    trainer.train()
+    # Save trained model
+    trainer.model.save_pretrained( f'{output_path}/{new_model_name}')
+
+    model_state_dict = model.state_dict()
+    torch.save(model_state_dict, f'{output_path}/{new_model_name}_state.pt')
+
+    print("Model state dictionary saved.")
+
+
+if __name__ == '__main__':
+
+    #Continual Pre-training part of code
+    base_model_name = 'microsoft/phi-2'
+    cpt_dataset_name = 'universeTBD/arxiv-astro-abstracts-all'
+    cpt_dataset_path = cpt_dataset_name
+  
+    # dataset = load_dataset(dataset_path)['train'].select(range(100))
+    cpt_dataset = load_from_disk(cpt_dataset_path)
+  
+    new_cpt_model_name = 'cpt_astrophi-full'
+    cpt_output_file_path = 'stored_output_model_cpt'
+    
+    if not os.path.exists(cpt_output_file_path):
+        os.makedirs(cpt_output_file_path, exist_ok = True)
+    
+    print("directories_made")
+    run_finetuning(cpt_dataset, base_model_name, new_cpt_model_name, cpt_output_file_path)
+
+    #Supervised finetuning part of code
+    
+    adapter_model_name = os.path.join(cpt_output_file_path, new_cpt_model_name)
+    sft_dataset_name = 'universeTBD/arxiv-astro-abstracts-all'
+    sft_dataset_path = cpt_dataset_name
+  
+    # dataset = load_dataset(dataset_path)['train'].select(range(100))
+    
+  
+    sft_output_file_path = 'stored_output_model_sft'
+    
+    if not os.path.exists(sft_output_file_path):
+        os.makedirs(sft_output_file_path, exist_ok = True)
+      
+    run_finetuning(cpt_dataset, adapter_model_name, new_cpt_model_name, sft_output_file_path)
+    
+    
