eb564c696c87cd7a61c9de074fc8c7c877313019
HardikArora17 <54908862+HardikArora17@users.noreply.github.com>
2024-05-12 01:31:49 +0530

    Update full_phi3_cpt.py

--- full_phi3_cpt.py
+++ full_phi3_cpt.py
@@ -1,78 +1,61 @@
 import torch
-from tqdm import trange
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments, Trainer
-from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training, get_peft_model
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer
 from trl import SFTTrainer
-from torch.utils.data import Dataset, random_split
 import pandas as pd
-from global_variables_phi2 import *
+from global_variables_phi3 import *
 import os
-from datasets import load_dataset, load_from_disk
-from datasets import Dataset as DS
-from accelerate import Accelerator
-import deepspeed
+from itertools import chain
+from datasets import load_dataset
 import json
 
-def run_finetuning(dataset, model_name, new_model_name, output_path):
+def group_texts(examples, block_size):
+    concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
     
-    #device_string = PartialState().process_index
-    #device_map = {'': device_string}
-    # Load tokenizer and model with QLoRA configuration
-    compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
+        
+    result = {
+        k: [t[i: i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    return result
 
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=use_4bit,
-        bnb_4bit_quant_type=bnb_4bit_quant_type,
-        bnb_4bit_compute_dtype=compute_dtype,
-        bnb_4bit_use_double_quant=use_nested_quant,
-    )
-
+def run_finetuning(raw_dataset, model_name, new_model_name, output_path):
+    
     tokenizer_path = model_name
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, use_fast = True)
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
     tokenizer.add_eos_token = True
+    print("Tokenizer Loaded")
     
-    print("tokenizer done")
-    class MyDataset(Dataset):
-            def __init__(self, txt_list, tokenizer, max_length):
-                self.input_ids = []
-                self.attn_masks = []
-                for example  in txt_list:
-                     txt = example['text']
-                     encodings_dict = tokenizer(txt, truncation=True, max_length=max_length, padding="max_length")
-                     self.input_ids.append(torch.tensor(encodings_dict['input_ids']))
-                     self.attn_masks.append(torch.tensor(encodings_dict['attention_mask']))
-            def __len__(self):
-                return len(self.input_ids)
-                                                                                    
-            def __getitem__(self, idx):
-                return self.input_ids[idx], self.attn_masks[idx]
 
+    def tokenize_function(examples):
+        return tokenizer(examples['text'], return_special_tokens_mask=True)
+    
+    tokenized_datasets = raw_dataset.map(
+            tokenize_function,
+            batched=True,
+            remove_columns=['text'],
+            desc="Running tokenizer on every text in dataset",
+        )
 
-    #dataset_x = MyDataset(dataset ,tokenizer,512)
-    #train_dataset, _ = random_split(dataset_x,[len(dataset_x),0])
-    #list_1 = []
-    #for eg in train_dataset:
-    #    list_1.append({'input_ids': eg[0], 'attention_mask':eg[1]})
-
-    #train_dataset = DS.from_list(list_1)
+    processed_dataset = tokenized_datasets.map(
+                group_texts,
+                fn_kwargs={'max_seq_length': block_size},
+                batched=True,
+                desc=f"Grouping texts in chunks of {block_size}",
+            )
+    
     print("DATA_LOADED")
-    print(dataset)
+    print(processed_dataset)
     
-    with open('ds_config_zero.json' ,'r') as out:
-        ds_config = json.load(out)
-
-    #acc= Accelerator()
-    model_path = model_name
-    model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code =True)
-    #model, _, _ ,_  = deepspeed.initialize(model=model, model_parameters = model.parameters(), config =ds_config)
-
-    #model = acc.prepare(model)    # model = model.to('cuda')
+    
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map = 'auto', trust_remote_code =True)
+   
     print("MODEL INITIALIZED")
     print("Model's parameters device:", next(model.parameters()).device)
-
-    # Alternatively, you can print the device of the model itself
     print("Model's device:", next(model.parameters()).device)
 
     model.config.use_cache = False
@@ -90,7 +73,6 @@ def run_finetuning(dataset, model_name, new_model_name, output_path):
         weight_decay=weight_decay,
         optim=optim,
         bf16=True,
-        deepspeed = 'ds_config_zero2.json',
         max_grad_norm=max_grad_norm,
         max_steps=max_steps,
         warmup_ratio=warmup_ratio,
@@ -102,20 +84,17 @@ def run_finetuning(dataset, model_name, new_model_name, output_path):
     print("LOADED TRAINING ARGUMENTS")
     
     # Set supervised fine-tuning parameters
-    trainer = SFTTrainer(
+    trainer = Trainer(
                 model=model,
-                train_dataset=dataset,
-                max_seq_length=512,
-                dataset_text_field='text',
+                train_dataset=processed_dataset,
                 tokenizer=tokenizer,
                 args=training_arguments)
-                #data_collator =lambda data: {'input_ids': torch.stack([f[0] for f in data]),
-                #                             'attention_mask': torch.stack([f[1] for f in data]),
-                #                             'labels': torch.stack([f[0] for f in data])})
-    trainer.train()
-    # Save trained model
-    trainer.model.save_pretrained( f'{output_path}/{new_model_name}')
+              
 
+    trainer.train()
+    trainer.model.save_pretrained( f'{output_path}/{new_model_name}_pre')
+    trainer.save_model(f'{output_path}/{new_model_name}')
+    
     model_state_dict = model.state_dict()
     torch.save(model_state_dict, f'{output_path}/{new_model_name}_state.pt')
 
@@ -124,12 +103,11 @@ def run_finetuning(dataset, model_name, new_model_name, output_path):
 
 if __name__ == '__main__':
     model_name = 'microsoft/Phi-3-mini-4k-instruct'
+    new_model_name = 'astrophi-3-mini-4k-instruct'
 
     dataset_name = 'KnightHardik/temp-astro-full-text'
-    dataset_path = dataset_name
-    dataset = load_dataset(dataset_path)['train']
-    # dataset = load_from_disk(dataset_path)
-    new_model_name = 'astrophi-3-mini-4k-instruct'
+    dataset = load_dataset(dataset_name)['train']
+    
     output_file_path = 'stored_output_model_cpt'
     
     if not os.path.exists(output_file_path):
